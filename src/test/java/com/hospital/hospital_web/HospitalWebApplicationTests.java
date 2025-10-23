1644beb1469cc6ad8e4bfa186aff60473f119daa
Lucas Oliveira Garcez <estudosengcomputacao@gmail.com>
2025-10-23 08:48:56 -0300

    feat: Initial commit

---
+++ src/test/java/com/hospital/hospital_web/HospitalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
